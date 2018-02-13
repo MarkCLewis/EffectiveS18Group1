56b64b053e417def978d103f20a6972389e9761f
Jonathan Fisher <jfisher1@trinity.edu>
2018-02-13 12:49:35 -0600

    Added graphics testing, currently nonfunctional

---
+++ src/main/java/graphicsTesting/TriangleMeshDemo.java
@@ -0,0 +1,71 @@
+package graphicsTesting;
+
+import javafx.application.Application;
+import javafx.scene.Camera;
+import javafx.scene.Group;
+import javafx.scene.PerspectiveCamera;
+import javafx.scene.Scene;
+import javafx.scene.shape.DrawMode;
+import javafx.scene.shape.MeshView;
+import javafx.scene.shape.TriangleMesh;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Material;
+import javafx.scene.paint.PhongMaterial;
+
+public class TriangleMeshDemo extends Application {
+	public static void main(String[] args) {
+		launch();
+	}
+	@Override
+	public void start(Stage stage) {
+		Group mainGroup = new Group();
+		Scene scene = new Scene(mainGroup, 1280, 720, true);
+		Camera camera = new PerspectiveCamera(true);
+		scene.setCamera(camera);
+		Group cameraGroup = new Group();
+		cameraGroup.getChildren().add(camera);
+		mainGroup.getChildren().add(cameraGroup);
+		
+		mainGroup.getChildren().add(getTestMesh());
+		
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+	
+	private MeshView getTestMesh() {
+		TriangleMesh pyramidMesh = new TriangleMesh();
+
+		pyramidMesh.getTexCoords().addAll(0,0);
+		
+		float h = 150;                    // Height
+		float s = 300;                    // Side
+		pyramidMesh.getPoints().addAll(
+		        0,    0,    0,            // Point 0 - Top
+		        0,    h,    -s/2,         // Point 1 - Front
+		        -s/2, h,    0,            // Point 2 - Left
+		        s/2,  h,    0,            // Point 3 - Back
+		        0,    h,    s/2           // Point 4 - Right
+		    );
+		
+		pyramidMesh.getFaces().addAll(
+		        0,0,  2,0,  1,0,          // Front left face
+		        0,0,  1,0,  3,0,          // Front right face
+		        0,0,  3,0,  4,0,          // Back right face
+		        0,0,  4,0,  2,0,          // Back left face
+		        4,0,  1,0,  2,0,          // Bottom rear face
+		        4,0,  3,0,  1,0           // Bottom front face
+		    );
+		
+		MeshView pyramid = new MeshView(pyramidMesh);
+		pyramid.setDrawMode(DrawMode.FILL);
+		pyramid.setMaterial(new PhongMaterial(Color.BLUE));
+		pyramid.setTranslateX(0/*200*/);
+		pyramid.setTranslateY(0/*100*/);
+		pyramid.setTranslateZ(0/*200*/);
+		
+		return pyramid;
+		
+	}
+}
