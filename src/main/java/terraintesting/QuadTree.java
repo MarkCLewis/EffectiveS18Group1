f58341e6f3890cbd3aef858f5c89e51c61d65cc6
dmatthe1 <dmatthe1@titan04.cs.trinity.edu>
2018-02-13 13:51:50 -0600

    added functions to quadTree

--- src/main/java/terraintesting/QuadTree.java
+++ src/main/java/terraintesting/QuadTree.java
@@ -1,22 +1,47 @@
 package terraintesting;
 
-public class QuadTree {
+class Node {
+	int value;
+	Node north;
+	Node east;
+	Node south;
+	Node west;
+	
+	Node (int value) {
+		this.value = value;
+		this.north = null;
+		this.east = null;
+		this.south = null;
+		this.west = null;
+	}
+}
 
+class Boundary {
+	
+	
+}
+	
+public class QuadTree {
 	private Node root;
 	
-	private class Node {
-		int value;
-		Node north;
-		Node east;
-		Node south;
-		Node west;
+	public boolean find (int value) {
 		
-		Node (int value) {
-			this.value = value;
-			this.north = null;
-			this.east = null;
-			this.south = null;
-			this.west = null;
-		}
+		return false;
+	}
+	
+	public static void insert (int value) {
+		
+	}
+	
+	public static void insert (Node node) {
+		
+	}
+	
+	public static Node delete (int value) {
+		return null;
+	}
+	
+	public static void display() {
+		
 	}
 }
