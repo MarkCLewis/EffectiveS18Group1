747e663acd78d289152d51e18cbf7090b28200e8
Jonathan Fisher <jfisher1@trinity.edu>
2018-02-27 13:51:59 -0600

    Fixed broken code, added mouseaim for camera

--- src/main/java/graphicsTesting/TriangleMeshDemo.java
+++ src/main/java/graphicsTesting/TriangleMeshDemo.java
@@ -2,6 +2,7 @@ package graphicsTesting;
 
 import javafx.application.Application;
 import javafx.scene.Camera;
+import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
@@ -16,11 +17,26 @@ import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Material;
 import javafx.scene.paint.PhongMaterial;
+import javafx.scene.transform.Rotate;
 
 public class TriangleMeshDemo extends Application {
 	public static void main(String[] args) {
 		launch();
 	}
+	
+	private double x1 = 0;
+	private double y1 = 0;
+	private double x2;
+    private double y2;
+    private double dx;
+    private double dy;
+    private Rotate xRotate = new Rotate(0,0,0,0,Rotate.X_AXIS);
+    private Rotate yRotate = new Rotate(0,0,0,0,Rotate.Y_AXIS);
+    private double rotateModifier = 25;
+    private double cameraYlimit = 15;
+    private double cameraFarClip = 10000;
+	private double cameraNearClip = 3;
+    
 	@Override
 	public void start(Stage stage) {
 		Group mainGroup = new Group();
@@ -29,6 +45,9 @@ public class TriangleMeshDemo extends Application {
 		
 		//Camera
 		Camera camera = new PerspectiveCamera(true);
+		camera.setFarClip(cameraFarClip);
+		camera.setNearClip(cameraNearClip);
+		camera.getTransforms().addAll(xRotate,yRotate);
 		scene.setCamera(camera);
 		Group cameraGroup = new Group();
 		cameraGroup.getChildren().add(camera);
@@ -43,27 +62,51 @@ public class TriangleMeshDemo extends Application {
 			 if(key == KeyCode.A) {camera.setTranslateX(camera.getTranslateX() - camSpeed);}
 			 if(key == KeyCode.D) {camera.setTranslateX(camera.getTranslateX() + camSpeed);}
 		});
-		
-		double x1;
-		double y2;
-		double x2 = event.getSceneX();
-        double y2 = event.getSceneY();
-        double dx;
-        double dy;
-	    scene.setOnMouseMoved((MouseEvent event) -> {
-	        if(pressed){
-	             x1 = x2;
-	             y1 = y2;
-	             x2 = event.getSceneX();
-	             y2 = event.getSceneY();
-	             dx = x2 -x1;
-	             dy = y2 -y1;
 
-	             camera.setTranslateX(camera.getTranslateX() - dx*0.1);
-	             camera.setTranslateY(camera.getTranslateY() - dy*0.1);      
-	        }           
+		scene.addEventHandler(MouseEvent.ANY, event -> {
+			if (event.getEventType() == MouseEvent.MOUSE_MOVED) {
+				x2 = event.getSceneX();
+				y2 = event.getSceneY();
+			 
+				//calculate the rotational change of the camera pitch
+				double pitchRotate =xRotate.getAngle()+(y2 - y1) / rotateModifier;
+				
+				//set min/max camera pitch to prevent camera flipping
+				pitchRotate = pitchRotate > cameraYlimit ? cameraYlimit : pitchRotate;
+				pitchRotate = pitchRotate < -cameraYlimit ? -cameraYlimit : pitchRotate;
+				
+				//replace the old camera pitch rotation with the new one.
+				xRotate.setAngle(pitchRotate);
+				
+				//calculate the rotational change of the camera yaw
+				double yawRotate=yRotate.getAngle()-(x2 - x1) / rotateModifier;
+				yRotate.setAngle(yawRotate);
+			
+				x1 = x2;
+				y1 = y2;
+				scene.setCursor(Cursor.NONE);
+		 	}
+		});
+/*		
+		scene.setOnMousePressed((MouseEvent event) -> {
+			pressed = true;
+	        x2 = event.getSceneX();
+	        y2 = event.getSceneY();
 	    });
 		
+	    scene.setOnMouseMoved((MouseEvent event) -> {
+	    	if(pressed) {
+	    		x1 = x2;
+	        	y1 = y2;
+	        	x2 = event.getSceneX();
+	        	y2 = event.getSceneY();
+	        	dx = x2 -x1;
+	        	dy = y2 -y1;
+	    	}
+	        camera.setTranslateX(camera.getTranslateX() - dx*0.1);
+	        camera.setTranslateY(camera.getTranslateY() - dy*0.1);      
+	    });
+	*/	
 		mainGroup.getChildren().add(getTestMesh());
 		
 		stage.setScene(scene);
