d2110d95f5161aad500564b36328d7b3e1f0fd7c
aramire4 <aramire4@atlas08.cs.trinity.edu>
2018-03-03 14:22:48 -0600

    Can make groups of houses

---
+++ src/main/java/citiesTesting/Shapes.java
@@ -0,0 +1,91 @@
+package citiesTesting;
+
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Cylinder;
+import javafx.scene.shape.DrawMode;
+import javafx.scene.shape.MeshView;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.TriangleMesh;
+
+
+public class Shapes {
+
+	
+	public static Cylinder makeCylinder(int w, int h, Color color1, Color color2, int x, int y, int z){
+		Cylinder cylinder = new Cylinder(w,h);
+		PhongMaterial material = new PhongMaterial();
+		material.setDiffuseColor(color1);
+		material.setSpecularColor(color2);
+		cylinder.setMaterial(material);
+		
+		cylinder.setTranslateX(x);
+		cylinder.setTranslateY(y);
+		cylinder.setTranslateZ(z);
+		return cylinder;
+		//mainGroup.getChildren().add(cylinder);
+	}
+	
+	public static Box makeBox(int l, int w, int h, Color color1, Color color2, int x, int y, int z){
+		Box box = new Box(100, 100, 100);
+		PhongMaterial material = new PhongMaterial();
+		material.setDiffuseColor(color1);
+		material.setSpecularColor(color2);
+		
+		box.setMaterial(material);
+		box.setTranslateX(x);
+		box.setTranslateY(y);
+		box.setTranslateZ(z);
+		return box;
+	}
+	
+	public static MeshView makePyramid(int h, int s, Color color1, Color color2, int x, int y, int z){
+		TriangleMesh pyramidMesh = new TriangleMesh();
+		PhongMaterial material = new PhongMaterial();
+		material.setDiffuseColor(color1);
+		material.setSpecularColor(color2);
+		
+		pyramidMesh.getTexCoords().addAll(0,0);
+		pyramidMesh.getPoints().addAll(
+		        0,    0,    0,            // Point 0 - Top
+		        0,    h,    -s/2,         // Point 1 - Front
+		        -s/2, h,    0,            // Point 2 - Left
+		        s/2,  h,    0,            // Point 3 - Back
+		        0,    h,    s/2           // Point 4 - Right
+		    );
+		
+		pyramidMesh.getFaces().addAll(
+		        0,0,  2,0,  1,0,          // Front left face
+		        0,0,  1,0,  3,0,          // Front right face
+		        0,0,  3,0,  4,0,          // Back right face
+		        0,0,  4,0,  2,0,          // Back left face
+		        4,0,  1,0,  2,0,          // Bottom rear face
+		        4,0,  3,0,  1,0           // Bottom front face
+		    );
+		MeshView pyramid = new MeshView(pyramidMesh);
+		pyramid.setDrawMode(DrawMode.FILL);
+		pyramid.setMaterial(material);
+		pyramid.setTranslateX(x);
+		pyramid.setTranslateY(y);
+		pyramid.setTranslateZ(z);
+		return pyramid;
+	}
+	
+	public static Rectangle makeRectangle(int w, int h, int x, int y, Color color1, Color color2){
+		Rectangle rect = new Rectangle();
+		//TODO-find out how to set color
+		/*
+		PhongMaterial material = new PhongMaterial();
+		material.setDiffuseColor(color1);
+		material.setSpecularColor(color2);
+		//rect.setMaterial(material);
+		*/
+		rect.setX(x);
+		rect.setY(y);
+		rect.setWidth(w);
+		rect.setHeight(h);
+		return rect;
+	}
+
+}
--- src/main/java/citiesTesting/ShapesDemo.java
+++ src/main/java/citiesTesting/ShapesDemo.java
@@ -1,15 +1,18 @@
 package citiesTesting;
 
 import javafx.application.Application;
-import javafx.scene.Camera;
 import javafx.scene.Group;
-import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
-import javafx.scene.shape.Cylinder;
-import javafx.stage.Stage;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Material;
 import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Cylinder;
+import javafx.scene.shape.DrawMode;
+import javafx.scene.shape.MeshView;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.TriangleMesh;
+import javafx.scene.transform.Rotate;
+import javafx.stage.Stage;
 
 public class ShapesDemo extends Application{
 	
@@ -30,21 +33,21 @@ public class ShapesDemo extends Application{
 		mainGroup.getChildren().add(cameraGroup);
 		*/
 /*
-		Cylinder cyl = makeCylinder(200, 100, Color.LIGHTBLUE, Color.BLUE, 350, 300, 200);
+		Cylinder cyl = Shapes.makeCylinder(200, 100, Color.LIGHTBLUE, Color.BLUE, 350, 300, 200);
 		mainGroup.getChildren().add(cyl);
 
-		//Cylinder cyl2 = makeCylinder(200, 100, Color.RED, Color.DARKRED, 900, 300, 200);
-		//mainGroup.getChildren().add(cyl2);
-
-		Cylinder cyl2 = makeCylinder(200, 100, Color.RED, Color.DARKRED, 250, 350, 0);
+		Cylinder cyl2 = Shapes.makeCylinder(200, 100, Color.RED, Color.DARKRED, 250, 350, 0);
 		mainGroup.getChildren().add(cyl2);
 */	
+		/*
+		//TODO
+		//Demo for cylinder houses
 		int myX = 1000;
 		int myY = 100;
 		int myZ = 600;
 		for(int i = 0; i < 6; i++){
-				//Cylinder cyl = makeCylinder(200, 100, Color.SANDYBROWN, Color.BROWN, myX, myY, myZ);
-				Cylinder cyl = makeCylinder(200, 100, Color.LIGHTBLUE, Color.BLUE, myX, myY, myZ);
+				//Cylinder cyl = Shapes.makeCylinder(200, 100, Color.SANDYBROWN, Color.BROWN, myX, myY, myZ);
+				Cylinder cyl = Shapes.makeCylinder(200, 100, Color.LIGHTBLUE, Color.BLUE, myX, myY, myZ);
 				mainGroup.getChildren().add(cyl);
 				myX -= 50;
 				myY += 50;
@@ -55,41 +58,85 @@ public class ShapesDemo extends Application{
 		myY = 100;
 		myZ = 600;
 		for(int i = 0; i < 6; i++){
-				//Cylinder cyl = makeCylinder(200, 100, Color.SANDYBROWN, Color.BROWN, myX, myY, myZ);
-				Cylinder cyl = makeCylinder(200, 100, Color.LIGHTBLUE, Color.BLUE, myX, myY, myZ);
+				//Cylinder cyl = Shapes.makeCylinder(200, 100, Color.SANDYBROWN, Color.BROWN, myX, myY, myZ);
+				Cylinder cyl = Shapes.makeCylinder(200, 100, Color.LIGHTBLUE, Color.BLUE, myX, myY, myZ);
 				mainGroup.getChildren().add(cyl);
 				myX -= 50;
 				myY += 50;
 				myZ -= 200;
 		}
+		*/
+		
+		/*
+		Rectangle r = Shapes.makeRectangle(100, 200, 500, 100, Color.LIGHTBLUE, Color.BLUE);
+		mainGroup.getChildren().add(r);
+		*/
+
+		/*
+		Box b = Shapes.makeBox(900, 900, 900, Color.LIGHTBLUE, Color.BLUE, 200, 250, 200);
+		b.setRotationAxis(Rotate.Y_AXIS);
+		b.setRotate(25);
+		mainGroup.getChildren().add(b);
+		
+		
+		MeshView p = Shapes.makePyramid(100, 250, Color.PALEVIOLETRED, Color.RED, 200, 100, 200);
+		mainGroup.getChildren().add(p);
+		*/
+		
+		/*
+		makeHouse(mainGroup, Color.PALEVIOLETRED, Color.RED, Color.LIGHTBLUE, Color.BLUE, 200, 250, 200); //Makes a house structure
+		makeHouse(mainGroup, Color.PALEVIOLETRED, Color.RED, Color.LIGHTBLUE, Color.BLUE, 450, 250, 200);
+		*/
+		/*
+		int temp = 200;
+		for(int i = 0; i < 4; i++){
+			makeHouse(mainGroup, Color.PALEVIOLETRED, Color.RED, Color.LIGHTBLUE, Color.BLUE, temp, 250, 200);
+			temp += 250;
+		}
+		*/
+		
+		//TODO
+		//Demo for basic houses in neighborhood
+		
+		int tempX = 450;
+		int tempY = 200;
+		int tempZ = 300;
+		for(int i = 0; i < 4; i++){
+			makeHouse(mainGroup, Color.PALEVIOLETRED, Color.RED, Color.LIGHTBLUE, Color.BLUE, tempX, tempY, tempZ);
+			tempX -= 100;
+			tempY += 100;
+			tempZ -= 200;
+		}
+		tempX = 850;
+		tempY = 200;
+		tempZ = 300;
+		for(int i = 0; i < 4; i++){
+			makeHouse(mainGroup, Color.PALEVIOLETRED, Color.RED, Color.LIGHTBLUE, Color.BLUE, tempX, tempY, tempZ);
+			tempX -= 100;
+			tempY += 100;
+			tempZ -= 200;
+		}
+		
+		/*
+		Cylinder cyl = Shapes.makeCylinder(200, 100, Color.LIGHTBLUE, Color.BLUE, 350, 300, 200);
+		mainGroup.getChildren().add(cyl);
+		*/
 		
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
 	}
 	
-	
-	public Cylinder makeCylinder(int w, int h, Color color1, Color color2, int x, int y, int z){
-		Cylinder cylinder = new Cylinder(w,h);
-		PhongMaterial blueStuff = new PhongMaterial();
-		blueStuff.setDiffuseColor(color1);
-		blueStuff.setSpecularColor(color2);
-		cylinder.setMaterial(blueStuff);
+	public void makeHouse(Group mg, Color roof1, Color roof2, Color house1, Color house2, int x, int y, int z){
+		Box b = Shapes.makeBox(900, 900, 900, house1, house2, x, y, z);
+		b.setRotationAxis(Rotate.Y_AXIS);
+		b.setRotate(25);
+		mg.getChildren().add(b);
 		
-		cylinder.setTranslateX(x);
-		cylinder.setTranslateY(y);
-		cylinder.setTranslateZ(z);
-		return cylinder;
-		//mainGroup.getChildren().add(cylinder);
-	}
-	
-	/*
-	public Group createBuildings(Group grp){
-		//Group bg = new Group();
-		
-		return bg;
+		MeshView p = Shapes.makePyramid(100, 250, roof1, roof2, x, y-150, z);
+		mg.getChildren().add(p);
+		//return temp;
 		
 	}
-	*/
-
+	
 }
