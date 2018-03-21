cb86a174b98f62270ed057eb4769586781c6aab4
Miranda Ward <Randa@mirandas-mbp.trinity.local>
2018-03-20 20:10:29 -0500

    Fractal Terrain doing stuff for diamond algorithm

--- src/main/java/terraintesting/FractalTerrain.java
+++ src/main/java/terraintesting/FractalTerrain.java
@@ -2,26 +2,76 @@ package terraintesting;
 
 public class FractalTerrain implements TerrainGenerationAlgorithm {
 	@Override
-	public void generateTerrain(int[][] heightMap, int maxElev) {
+	public void generateTerrain(double[][] heightMap, double maxElev) {
 		//TODO
+		//make methods static???
+
+		//count of what iteration it is (matters for roughness)
+		private double iter = 0.0;
+
+		//generates random roughness coefficient; can be changed for control
+		private double randCoeff = new Random().nextDouble(1.0);
+		//private double randCoeff = 0.5 //for testing
+
+		//generates peturbation; -roughness^n <= peturbation <= roughness^n
+		//where n is the iteration
+		private void generatePeturb(double iter){
+			private int pet = (-(Math.pow(randCoeff, iter))) +
+					Math.random.nextDouble(Math.pow(randCoeff, (2 * iter));
+		}
+
+		//generates random height to assign to points
+		generateRandom(){
+			//does this need to be a double?
+			private double rand = new Random().nextDouble(maxElev);
+		}
+
+		//assigns the random heights from 0 to the maxElev
+		assignHeights(double x, double y, double squareWidth){
+			heightMap[x][y] = generateRandom(); //x1
+			heightMap[x + squareWidth][y] = generateRandom(); //x2
+			heightMap[x][y + squareWidth] = generateRandom(); //x3
+			heightMap[x + squareWidth][y + squareWidth] = generateRandom(); //x4
+		}
+
+		//createNewCorners(){}
+
+		private double avgCorners(double x, double y){
+			private double average = ((heightMap[x][y] + heightMap[x + squareWidth][y] +
+					  heightMap[x][y + squareWidth] +
+					  heightMap[x + squareWidth][y + squareWidth])/4);
+			oAvg = average + peturb;
+			return oAvg; //???
+		}
+
+		private void diamondStep(double x, double y, double squareWidth){
+			heightMap[x + (squareWidth/2)][y + (squareWidth/2)] = avgCorners(x,y);
+		}
+
+		//squareStep(double x, double y){}
+
+		//diamond and square step get repeated over and over
+
+		//recursiveFunction(){
+			//if ()
+			//diamondStep
+			//squareStep
+		//}
 	}
-	
-	
-	//Old code we might be able to salvage: 
+
+	//Old code we might be able to salvage:
 	/*
 	/*
 	 * diamond square algorithm to construct terrain (works with squares only)
 	 * @ param squareWidth - the width of the square you're making terrain with
 	 * @ param ulX - the upperleft hand corner of the square you're making terrain with
 	 * @ param ulY - the upperright hand corner of the square you're making terrain with
-	
+
 	public static void buildFractalElevation(int squareWidth, int ulX, int ulY) {
 		if (squareWidth >= 2) {
 			diamondStep(squareWidth, ulX, ulY);
 			//TODO
-			
 		}
-		
 	}
 
 	private static void diamondStep(int squareWidth, int ulX, int ulY) {
@@ -29,35 +79,35 @@ public class FractalTerrain implements TerrainGenerationAlgorithm {
 		elevation[ulX][ulY + squareWidth] = randomElevation();
 		elevation[ulX + squareWidth][ulY] = randomElevation();
 		elevation[ulX + squareWidth][ulY + squareWidth] = randomElevation();
-		
-		int averageElevation = (elevation[ulX][ulY] + 
-								elevation[ulX][ulY + squareWidth] + 
+
+		int averageElevation = (elevation[ulX][ulY] +
+								elevation[ulX][ulY + squareWidth] +
 								elevation[ulX + squareWidth][ulY] +
 								elevation[ulX + squareWidth][ulY + squareWidth]) / 4;
-		
+
 		//TODO
 		int peturbation = 0;
 		elevation[ulX + squareWidth/2][ulY + squareWidth/2] = averageElevation + peturbation;
 	}
-	
+
 	public static void squareStep(int squareWidth, int ulX, int ulY) {
 		//TODO
-		
+
 	}
 
-    
+
      * tesselates the square domain into a triangular grid because triangles are always coplaner
-     * @param 
-     
+     * @param
+
     public static void triangleRender(int x1, int y1, int x2, int y2, int x3, int y3) {
-    	//randomly set heights of initial triangle corner 
+    	//randomly set heights of initial triangle corner
     	if(elevation[x1][y1] == 0)
     		elevation[x1][y1] = randomElevation();
     	if(elevation[x2][y2] == 0)
     		elevation[x2][y2] = randomElevation();
     	if(elevation[x3][y3] == 0)
     		elevation[x3][y3] = randomElevation();
-    	
+
     	if(Math.abs(x1-x2)>1 && Math.abs(x2-x3)>1) {
     		//take midpoints
         	int x1_2 = (x1+x2)/2;
@@ -66,31 +116,31 @@ public class FractalTerrain implements TerrainGenerationAlgorithm {
         	int y1_3 = (y1+y3)/2;
         	int x2_3 = (x2+x3)/2;
         	int y2_3 = (y2+y3)/2;
-        	
+
         	//set heights of midpoints
-        	int avg = average(elevation[x1][y1], elevation[x2][y2], elevation[x3][y3]); 
-        	
+        	int avg = average(elevation[x1][y1], elevation[x2][y2], elevation[x3][y3]);
+
         	elevation[x1_2][y1_2] = maxElevation % (avg + randomElevation()/2);
         	elevation[x1_3][y1_3] = maxElevation % (avg + randomElevation()/2);
         	elevation[x2_3][y2_3] = maxElevation % (avg + randomElevation()/2);
-        	
+
     		triangleRender(x1, y1, x1_2, y1_2, x1_3, y1_3);
 	    	triangleRender(x1, y1, x1_2, y1_2, x1_3, y1_3);
 	    	triangleRender(x1, y1, x1_2, y1_2, x1_3, y1_3);
     	}
     }
-	
+
     private static int average(int ... nums) {
     	int sum = 0;
     	for(int i:nums)
     		sum+=i;
     	return sum/nums.length;
     }
-    
-	
+
+
 	 *  returns a random elevation between 0 and the maxElevation
 	 *  @ return an elevation
-	 
+
 	public static int randomElevation() {
 		return (int) (maxElevation * Math.random());
 	}
