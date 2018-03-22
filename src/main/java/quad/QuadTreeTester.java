86c79efaae29be2b194543a0b3dcfee9c315d74f
dmatthe1 <dmatthe1@titan04.cs.trinity.edu>
2018-03-22 13:59:02 -0500

    adding to test

--- src/main/java/quad/QuadTreeTester.java
+++ src/main/java/quad/QuadTreeTester.java
@@ -1,14 +1,37 @@
 package quad;
 
 import virtualworld.ExampleObject;
+import virtualworld.WorldObject;
 
 public class QuadTreeTester {
-	QuadTree quad = new QuadTree();
 	
-	ExampleObject testOb = new ExampleObject(0, 0, 5);
 	
-	//quad.insert(testOb);
+	public static QuadTree addToTree() {
+		QuadTree quad = new QuadTree();
+		
+		WorldObject testOb1 = new ExampleObject(0, 0, 5);
+		WorldObject testOb2 = new ExampleObject(0, 0, 5);
+		WorldObject testOb3 = new ExampleObject(0, 0, 5);
+		WorldObject testOb4 = new ExampleObject(0, 0, 5);
+		WorldObject testOb5 = new ExampleObject(0, 0, 5);
+		WorldObject testOb6 = new ExampleObject(0, 0, 5);
+		WorldObject testOb7 = new ExampleObject(0, 0, 5);
 	
-	PrintVisitor printTest = new PrintVisitor();
+		quad.insert(testOb1);
+		quad.insert(testOb2);
+		quad.insert(testOb3);
+		quad.insert(testOb4);
+		quad.insert(testOb5);
+		quad.insert(testOb6);
+		quad.insert(testOb7);
 	
+		return quad;
+	}
+	
+	
+		PrintVisitor printTest = new PrintVisitor();
+	
+	public static void main(String[] args) {
+		
+	}
 }
