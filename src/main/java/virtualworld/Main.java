8728b90fdac2b71518452488c07a80a1644da777
Jonathan Fisher <jfisher1@trinity.edu>
2018-04-17 12:48:05 -0500

    Small changes to main

--- src/main/java/virtualworld/Main.java
+++ src/main/java/virtualworld/Main.java
@@ -1,10 +1,15 @@
 package virtualworld;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import graphicsTesting.CameraController;
 import javafx.application.Application;
 import javafx.scene.Camera;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Material;
 import javafx.scene.paint.PhongMaterial;
@@ -22,6 +27,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		//Create group, scene, and camera
 		primaryStage.setTitle("Virtual World");
 		Group mainGroup = new Group();
 		Scene scene = new Scene(mainGroup, 1280, 720, true);
@@ -30,7 +36,61 @@ public class Main extends Application {
 		Group cameraGroup = new Group();
 		cameraGroup.getChildren().add(camera);
 		mainGroup.getChildren().add(cameraGroup);
+		
+		//Create CameraController pCam
+		CameraController pCam = new CameraController.Builder(camera).build();
+		
+		//Camera Movement
+		Set<KeyCode> keySet = new HashSet<KeyCode>();
+		scene.setOnKeyPressed(event ->{ 
+			 KeyCode key = event.getCode();
+			 keySet.add(key);
+			 
+			 if(keySet.contains(KeyCode.W)) {
+				 pCam.moveForward();
+			 }
+			 if(keySet.contains(KeyCode.S)) {
+				 pCam.moveBackward();
+			 }
+			 if(keySet.contains(KeyCode.A)) {
+				 pCam.moveLeft();
+			 }
+			 if(keySet.contains(KeyCode.D)) {
+				 pCam.moveRight();
+			 }
+			 if(keySet.contains(KeyCode.RIGHT)) {
+				 pCam.rotateRight();
+			 }
+			 if(keySet.contains(KeyCode.LEFT)) {
+				 pCam.rotateLeft();
+			 }
+			 if(keySet.contains(KeyCode.UP)) {
+				 pCam.rotateUp();
+			 }
+			 if(keySet.contains(KeyCode.DOWN)) {
+				 pCam.rotateDown();
+			 }
+			 if(keySet.contains(KeyCode.R)) {
+				 pCam.moveUp();
+			 }
+			 if(keySet.contains(KeyCode.F)) {
+				 pCam.moveDown();
+			 }
+			 if(keySet.contains(KeyCode.SHIFT)) {
+				 pCam.boostOn();
+			 }
+		});
+		
+		scene.setOnKeyReleased(event ->{
+			KeyCode key = event.getCode();
+			if(key == KeyCode.SHIFT) {
+				pCam.boostOff();
+			}
+			keySet.remove(key);
+			
+		});
 
+		//Test shapes
 		Sphere sphere = new Sphere(2);
 		Material mat = new PhongMaterial(Color.BLUE);
 		sphere.setMaterial(mat);
