6759850b7fbb9b36eafd6861e0bd0ab987911c35
Colby Doyal <cdoyal@trinity.edu>
2018-04-24 11:58:26 -0500

    Better name

--- src/main/java/animals/boxMove.java
+++ src/main/java/animals/boxMove.java
@@ -19,7 +19,7 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 
-public class Triad extends Application {
+public class boxMove extends Application {
 
     private static final double SIZE = 300;
     private final Content content = Content.create(SIZE);
