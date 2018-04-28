021d37dcb2700cf8cec38435a857a771c4d65b1c
Miranda Ward <Randa@mirandas-mbp.trinity.local>
2018-04-28 16:27:24 -0500

    added to Dynamic Loading

--- src/main/java/terraintesting/DynamicLoading.java
+++ src/main/java/terraintesting/DynamicLoading.java
@@ -4,16 +4,17 @@ package terraintesting;
 
 public class DynamicLoading {
 	long seed = 0x327a96;
-	
+
 	long posRandomLong(double x, double y) {
 		long lx = Double.doubleToLongBits(x);
 		long ly = Double.doubleToLongBits(y);
-		
+
 		return lx ^ ly ^ (seed & (lx >> 32)) ^ (seed & (lx << 32)) ^ (seed & (ly >> 32)) ^ (seed & (ly << 32));
 	}
-	
+
 	double posRandomDouble(double x, double y) {
-		
+		double dx = Long.doubleValue(x);
+		double dy = Long.doubleValue(y);
 		return 0.0;
 	}
 }
