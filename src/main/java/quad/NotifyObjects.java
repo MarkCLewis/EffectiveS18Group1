3dec64348f247ea625f54db325a000fb08f4c144
dmatthe1 <dmatthe1@trinity.edu>
2018-05-05 11:46:21 -0500

    main

--- src/main/java/quad/NotifyObjects.java
+++ src/main/java/quad/NotifyObjects.java
@@ -43,7 +43,7 @@ public class NotifyObjects implements ElementVisitor {
   		double xLoc = item.getXLoc();
   		double zLoc = item.getZLoc();
   		double itemR = item.getSize();
-  		double camR = 2000;
+  		double camR = 300;
   		
   		double distance = Math.sqrt(Math.pow((xLoc - xCam), 2) + Math.pow((zLoc - zCam), 2));
   		return (camR > (distance + itemR));
